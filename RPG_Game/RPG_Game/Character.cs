e38708114624a75e57a21d56eaca148910756a62
yehorzvoznikov-hash <yehorzvoznikov@gmail.com>
2025-12-04 12:59:28 +0200

    Make Character script

---
+++ RPG_Game/RPG_Game/Character.cs
@@ -0,0 +1,64 @@
+﻿namespace RPG_Game
+{
+    public abstract class Character
+    {
+        private int _health;
+        private int _maxHealth;
+        private int _strength;
+        private string _name;
+
+        public string Name
+        {
+            get => _name;
+            protected set => _name = value;
+        }
+
+        public int Health
+        {
+            get => _health;
+            protected set => _health = Math.Max(0, Math.Min(0, _maxHealth));
+        }
+
+        public int MaxHealth
+        {
+            get => _maxHealth;
+            protected set => _maxHealth = value;
+        }
+
+        public int Strength
+        {
+            get => _strength; 
+            protected set => _strength = Math.Max(1, value);
+        }
+
+        public bool IsAlive => _health > 0;
+
+        protected Character(int health, int strength, string name)
+        {
+            Health = health;
+            MaxHealth = health;
+            Strength = strength;
+            Name = name;
+        }
+
+        public abstract void Attack(Character target);
+
+        public virtual void TakeDamage(int damage)
+        {
+            Health -= damage;
+            Console.WriteLine($"{Name} отримав {damage} пошкоджень! (HP: {Health}/{MaxHealth})");
+
+            if (!IsAlive)
+            {
+                Console.WriteLine($"{Name} загинув!");
+            }
+        }
+
+        public void Heal(int amout)
+        {
+            var oldHealth = Health;
+            Health += amout;
+            Console.WriteLine($"{Name} відновив {Health - oldHealth} (HP: {Health}/{MaxHealth})");
+        }
+    }
+}
--- RPG_Game/RPG_Game/Program.cs
+++ RPG_Game/RPG_Game/Program.cs
@@ -1,12 +1,5 @@
 ﻿namespace RPG_Game
 {
-    public abstract class Character
-    {
-        private int _health;
-        private int _maxHealth;
-        private int _strength;
-        private string _name;
-    }
 
     internal class Program
     {
