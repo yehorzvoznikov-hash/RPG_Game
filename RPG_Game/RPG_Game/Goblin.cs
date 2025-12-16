79ac7ce9a8dba8ae09ba224bb52d92c55ab9103b
yehorzvoznikov-hash <yehorzvoznikov@gmail.com>
2025-12-16 12:09:05 +0200

    Add DarkMage

---
+++ RPG_Game/RPG_Game/Goblin.cs
@@ -0,0 +1,17 @@
+﻿namespace RPG_Game
+{
+    public class Goblin : Enemy
+    {
+        public Goblin() : base(40, 8, "Гоблін", 30)
+        {
+
+        }
+
+        public override void Attack(Character target)
+        {
+            var damage = Strength;
+            Console.WriteLine($"{Name} б'є кинджалом і завдає {damage} пошкоджень!");
+            target.TakeDamage(damage);
+        }
+    }
+}
--- RPG_Game/RPG_Game/Program.cs
+++ RPG_Game/RPG_Game/Program.cs
@@ -1,20 +1,35 @@
 ﻿namespace RPG_Game
 {
-    public class Goblin : Enemy
+    public class DarkMage : Enemy, ISpellCaster
     {
-        public Goblin() : base(40, 8, "Гоблін", 30)
-        {
+        private int _mana;
 
+        public int Mana
+        {
+            get => _mana;
+            private set => _mana = Math.Max(0, value);
+        }
+
+        public DarkMage() : base(60, 10, "Dark Mage", 80)
+        {
+            _mana = 100;
         }
 
         public override void Attack(Character target)
         {
-            var damage = Strength;
-            Console.WriteLine($"{Name} б'є кинджалом і завдає {damage} пошкоджень!");
-            target.TakeDamage(damage);
+            throw new NotImplementedException();
+        }
+
+        public void CastSpell(Character target)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RestoreMana(int amount)
+        {
+            Mana += amount;
         }
     }
-
 
     internal class Program
     {
